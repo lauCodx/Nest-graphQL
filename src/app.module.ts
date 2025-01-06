e446b3ee8775fac8a64fcb288aad91063e1096bf
lauCodx <lauphix1@gmail.com>
2025-01-06 21:15:05 +0100

    Added a database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PetModule } from './pet/pet.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
@@ -12,6 +13,12 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts, .js}'],
+      synchronize: true
     })
     ,PetModule],
   controllers: [AppController],
